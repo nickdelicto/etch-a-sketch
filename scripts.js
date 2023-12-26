a92ef00d4a14463f6fe86b28c3d5d865cda47cb0
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2023-12-26 15:47:24 +0300

    Set up pixelated hover effect on grid
    Using an event listener to change grid background color on hover.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,9 @@ const containerDiv = document.querySelector('.container');
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.setAttribute('class', 'squareDiv');
+    squareDiv.addEventListener('mouseover', (e) => {
+        e.target.setAttribute('style', 'background-color: yellow;');
+    } );
     containerDiv.appendChild(squareDiv);
 }
+
