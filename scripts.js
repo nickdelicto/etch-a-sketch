0e41b681a28f8c526540dee52bf85f046a02b801
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2023-12-26 19:44:15 +0300

    Allow user to specify number of squares in grid
    Makes the grid dynamic depending on the number of squares per
    grid side entered by user.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
 const containerDiv = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class', 'squareDiv');
-    squareDiv.addEventListener('mouseover', (e) => {
-        e.target.setAttribute('style', 'background-color: yellow;');
-    } );
-    containerDiv.appendChild(squareDiv);
-}
+const button = document.querySelector('button');
+
+let squares;
+
+button.addEventListener('click', () => {
+    containerDiv.innerHTML = '';
+    do {
+        squares = +prompt('how many squares do you want for each side of the grid?');
+    } while (squares > 100);
+
+    for (let i = 0; i < squares; i++) {
+        for (let j = 0; j < squares; j++) {
+            const squareDiv = document.createElement('div');
+            squareDiv.setAttribute('class', 'squareDiv');
+            squareDiv.addEventListener('mouseover', (e) => {
+                e.target.setAttribute('style', 'background-color: yellow;');
+            });
+            containerDiv.appendChild(squareDiv);    
+        }  
+    }
+});
+
+
+
+
 
