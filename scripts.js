a1c1fe5198bbeb6c616ec8e676f2ea8619549eb9
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2023-12-26 13:09:09 +0300

    Add class attribute to dynamically created divs
    This is to help with css targeting

--- scripts.js
+++ scripts.js
@@ -2,5 +2,6 @@ const containerDiv = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
+    squareDiv.setAttribute('class', 'squareDiv');
     containerDiv.appendChild(squareDiv);
 }
